11d9f395f8d040b606837041385c051322418adf
llensi <lucas.lensi@gmail.com>
2019-11-25 18:58:55 +0100

    create cart when creating user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
-  
+  before_action :current_cart
+
+private
+  def current_cart
+     if current_user && current_user.cart == nil
+      @current_cart = Cart.create(user: current_user)
+      current_user.cart = @current_cart
+    end
+  end
 end
