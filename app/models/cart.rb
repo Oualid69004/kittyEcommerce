cd80f14736b3622000f67a9757345500f2e2cd68
llensi <lucas.lensi@gmail.com>
2019-11-25 16:41:56 +0100

    delete some conflict

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,13 +1,8 @@
 class Cart < ApplicationRecord
-<<<<<<< HEAD
-  belongs_to :user
-  has_many :items
-  belongs_to :item
-=======
+
   belongs_to :user, optional: true
   #has_many :items
   belongs_to :item, optional: true
->>>>>>> 4444c6ddf3f3ae928c538d63725ea7fc04bb0c69
 
 
 end
