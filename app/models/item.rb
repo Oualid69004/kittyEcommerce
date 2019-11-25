58a90f040195725d615d27fb88471ec6b7dfb688
sarra <sarra.bouras1@gmail.com>
2019-11-25 16:25:49 +0100

    s

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
-    validates :title, presence: true,
-    validates :description, length: { minimum: 200 },
+    validates :title, presence: true
+    validates :description, length: { minimum: 200 }
 
     has_many :orders
     has_many  :users, through: :orders
