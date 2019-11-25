a9f98a2727ee12cf492d00850d30ae80e3d8cba1
walid <wmersaoui@gmail.com>
2019-11-25 14:32:34 +0100

    mmm

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  i = Item.new(title: Faker::Address.street_address,
+                   price: rand(600..5000),
+                   description: Faker::Address.full_address)
+  i = Image.new(item_id: i.id, description: 'Super appart', url: 'https://s-ec.bstatic.com/images/hotel/max1024x768/716/71622578.jpg')
+  i.save
+  p.save
+  puts "Property n° #{p.id} created"
+end
