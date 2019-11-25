b5c76f1e713218d24f3430b04453c6c36042994b
sashiiredev <alishiret@gmail.com>
2019-11-25 17:13:13 +0100

    modif table order

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
     validates :title, presence: true,
-    validates :description, length: { minimum: 200 },
+    validates :description, length: { minimum: 200 }
 
     has_many :orders
     has_many  :users, through: :orders
