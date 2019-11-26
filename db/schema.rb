0756644b07c1ce4b21be04a3d6490f2ee2ee6f2c
sarra <sarra.bouras1@gmail.com>
2019-11-26 11:28:05 +0100

    mail en devlp

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_11_25_092201) do
-=======
-
 ActiveRecord::Schema.define(version: 2019_11_25_131927) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> master
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "item_id"
@@ -34,8 +24,24 @@ ActiveRecord::Schema.define(version: 2019_11_25_131927) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
-<<<<<<< HEAD
-=======
+  create_table "item_to_carts", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_item_to_carts_on_cart_id"
+    t.index ["item_id"], name: "index_item_to_carts_on_item_id"
+  end
+
+  create_table "itemcarts", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_itemcarts_on_cart_id"
+    t.index ["item_id"], name: "index_itemcarts_on_item_id"
+  end
+
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -54,29 +60,18 @@ ActiveRecord::Schema.define(version: 2019_11_25_131927) do
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
->>>>>>> master
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-<<<<<<< HEAD
-=======
     t.bigint "cart_id"
->>>>>>> master
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
-=======
     t.index ["cart_id"], name: "index_users_on_cart_id"
->>>>>>> master
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-<<<<<<< HEAD
-=======
->>>>>>> 6e75a8c465c5006d222db39e1e9700a73c381297
->>>>>>> master
 end
