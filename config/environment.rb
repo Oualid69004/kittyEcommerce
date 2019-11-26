fabedf7323e3fe6279f1bd326e61eeec5f210ceb
walid <wmersaoui@gmail.com>
2019-11-27 00:20:23 +0100

    env

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 require 'dotenv'
-Dotenv.load ('.env')
+Dotenv.load 
 
 # Initialize the Rails application.
 Rails.application.initialize!
