5e95ea3c8a3e5d68e09ea27372d62c667bd66a1d
sashiiredev <alishiret@gmail.com>
2019-11-27 12:39:24 +0100

    modifié item_index_rom.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -17,18 +17,11 @@ ActiveRecord::Schema.define(version: 2019_11_26_153747) do
 
   create_table "carts", force: :cascade do |t|
     t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_carts_on_user_id"
-  end
-
-  create_table "item_to_carts", force: :cascade do |t|
-    t.bigint "cart_id"
     t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_item_to_carts_on_cart_id"
-    t.index ["item_id"], name: "index_item_to_carts_on_item_id"
+    t.index ["item_id"], name: "index_carts_on_item_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "itemcarts", force: :cascade do |t|
@@ -60,8 +53,10 @@ ActiveRecord::Schema.define(version: 2019_11_26_153747) do
 
   create_table "orders", force: :cascade do |t|
     t.bigint "user_id"
+    t.bigint "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -72,10 +67,6 @@ ActiveRecord::Schema.define(version: 2019_11_26_153747) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
