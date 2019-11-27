4b7ffe971d001da6559af360fe3f9ce7e1fc88dc
walid <wmersaoui@gmail.com>
2019-11-27 09:45:20 +0100

    env

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,12 @@
 # Load the Rails application.
 require_relative 'application'
-
+Dotenv.load 
 
 # Initialize the Rails application.
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    Dotenv.load 
+   
     :user_name => ENV["SENDGRID_LOGIN"],
     :password => ENV["SENDGRID_PWD"],
     :domain => 'monsite.fr',
