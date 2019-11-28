bea31b7c1ae5f5f7669d8bb125af6211357868a2
llensi <lucas.lensi@gmail.com>
2019-11-28 22:27:32 +0100

    add photo cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_26_153747) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,13 +36,6 @@ ActiveRecord::Schema.define(version: 2019_11_26_153747) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "carts", force: :cascade do |t|
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_carts_on_user_id"
-  end
-
   create_table "item_to_carts", force: :cascade do |t|
     t.bigint "cart_id"
     t.bigint "item_id"
@@ -50,59 +43,6 @@ ActiveRecord::Schema.define(version: 2019_11_26_153747) do
     t.datetime "updated_at", null: false
     t.index ["cart_id"], name: "index_item_to_carts_on_cart_id"
     t.index ["item_id"], name: "index_item_to_carts_on_item_id"
-  end
-
-  create_table "itemcarts", force: :cascade do |t|
-    t.bigint "cart_id"
-    t.bigint "item_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["cart_id"], name: "index_itemcarts_on_cart_id"
-    t.index ["item_id"], name: "index_itemcarts_on_item_id"
-  end
-
-  create_table "items", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.float "price"
-    t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "orderitems", force: :cascade do |t|
-    t.bigint "order_id"
-    t.bigint "item_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_orderitems_on_item_id"
-    t.index ["order_id"], name: "index_orderitems_on_order_id"
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_orders_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.bigint "cart_id"
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "admin"
-    t.index ["cart_id"], name: "index_users_on_cart_id"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
